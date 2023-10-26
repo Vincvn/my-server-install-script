851449dac625caf3ebfd4873e17b69b346fc4fd7
Vincent Hoang <toanhdcnet@gmail.com>
2023-10-26 14:28:56 +0700

    Update firewalld-alias.sh

--- firewalld-alias.sh
+++ firewalld-alias.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 # Define the aliases
-alias_remove="alias fwd-rm='sudo firewall-cmd --zone=public --permanent --remove-port='"
-alias_add="alias fwd-add='sudo firewall-cmd --zone=public --permanent --add-port='"
-
+alias="alias fwd='sudo firewall-cmd --zone=public --permanent'"
 # Check if the aliases already exist in ~/.zshrc
-if grep -Fxq "$alias_remove" ~/.zshrc && grep -Fxq "$alias_add" ~/.zshrc; then
+if grep -Fxq "$alias" ~/.zshrc; then
     echo "Aliases already exist in ~/.zshrc"
 else
     # Append the aliases to ~/.zshrc if they don't exist
     echo "\n# Custom Aliases for firewall-cmd" >> ~/.zshrc
-    echo "$alias_remove" >> ~/.zshrc
-    echo "$alias_add" >> ~/.zshrc
+    echo "$alias" >> ~/.zshrc
     echo "Aliases added to ~/.zshrc"
 fi
